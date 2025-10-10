7f844c721acbb5b05e97e4f5e8a506ff4d0e6fc5
mr <mr>
2025-10-10 22:00:23 +0800

    rename transport udp

--- uros/platform.h
+++ uros/platform.h
@@ -4,6 +4,12 @@
 
 #include "uros_config.h"
 
+#ifdef UROS_VERBOSE
+#define UROS_PRINT(...) printf(...)
+#else
+#define UROS_PRINT(...)
+#endif
+
 #ifdef PLATFORM_LINUX
 #include "platform_linux.h"
 #endif
